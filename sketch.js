05c581f35437c0b75e780f99b7080eb6d593b614
ProSniper729 <76519132+ProSniper729@users.noreply.github.com>
2021-01-29 20:33:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,15 +7,15 @@ var mouse,mouseImg1,mouseImg2,mouseImg3;
 
 function preload() {
     //load the images here
-    gardenImg = loadImage("images/garden.png");
+    gardenImg = loadImage("garden.png");
 
-    catImg1 = loadImage("images/cat1.png");
-    catImg2 = loadAnimation("images/cat2.png","images/cat3.png")
-    catImg3 = loadAnimation("images/cat4.png");
+    catImg1 = loadImage("cat1.png");
+    catImg2 = loadAnimation("cat2.png","cat3.png")
+    catImg3 = loadAnimation("cat4.png");
     
-    mouseImg1=loadImage("images/mouse1.png");
-    mouseImg2=loadAnimation("images/mouse2.png","images/mouse3.png");
-    mouseImg3=loadAnimation("images/mouse4.png");
+    mouseImg1=loadImage("mouse1.png");
+    mouseImg2=loadAnimation("mouse2.png","mouse3.png");
+    mouseImg3=loadAnimation("mouse4.png");
 
     
     
